03f0081492d8545781f6c68e42bc26865316cd2d
Cody Sommer <codisimus@gmail.com>
2017-01-28 16:14:10 -0500

    Off by 1 after removing # from hash

--- js/bigbowlbingo.js
+++ js/bigbowlbingo.js
@@ -54,7 +54,7 @@ function navigateToHash() {
   }
   if (hash.length >= bingoBoardSize) {
     var charArray = hash.split("");
-    for (var i = 1; i <= bingoBoardSize; i++) {
+    for (var i = 0; i < bingoBoardSize; i++) {
       var select = document.getElementById(i);
       select.value = charArray[i];
       select.classList.add("readOnly");
