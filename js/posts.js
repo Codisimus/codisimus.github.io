959a555a558d6b5c5f3577b5ca75a030052886b2
Cody Sommer <codisimus@gmail.com>
2023-08-09 16:11:27 -0400

    Added IFTTT

--- js/posts.js
+++ js/posts.js
@@ -1,7 +1,7 @@
 function loadPosts() {
   loadPost("post" + 1, "https://raw.githubusercontent.com/Codisimus/codisimus.github.io/master/README.md");
   loadPost("post" + 2, "https://raw.githubusercontent.com/Codisimus/Tesla-Wall-Connector-Logger/master/README.md");
-  loadPost("post" + 3, "https://raw.githubusercontent.com/Codisimus/codisimus.github.io/master/posts/2023-08-08%20Initial%20Post.md");
+  loadPost("post" + 3, "https://raw.githubusercontent.com/Codisimus/AutoIFTTT/master/README.md");
   loadPost("post" + 4, "https://raw.githubusercontent.com/Codisimus/codisimus.github.io/master/posts/2023-08-08%20Initial%20Post.md");
   loadPost("post" + 5, "https://raw.githubusercontent.com/Codisimus/codisimus.github.io/master/posts/2023-08-08%20Initial%20Post.md");
 };
